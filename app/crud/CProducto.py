8c3f9d551d86b5f62b44dcd097d7b2cb3cda0a82
juan-fdo <arameo100@hotmail.com>
2022-11-07 18:16:41 -0500

    Corrección de nombres e importación.

---
+++ app/crud/CProducto.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Session
+from schemas import SProducto
+from models.Producto import Producto
+
+def create(db: Session, producto: SProducto.Create):
+	db_producto = Producto(nombre = producto.nombre, precio = producto.precio)
+	db.add(db_producto)
+	db.commit()
+	db.refresh(db_producto)
+	return db_producto
+
+def get(db: Session, id: int):
+	return db.query(Producto).filter(Producto.id == id).first()
