78a794273c8a7aefd7b533c3e9386f2ea1a05f41
juan-fdo <arameo100@hotmail.com>
2022-11-07 16:44:47 -0500

    Se agregan los campos al modelo de producto

--- app/models/Producto.py
+++ app/models/Producto.py
@@ -1,4 +1,7 @@
+from sqlalchemy import Column,Integer,String
+from db.database import Base
 class Producto(Base):
 	__tablename__ = 'producto';
 	id = Column(Integer, primary_key = True, index = True)
-	name = Column(String)
+	nombre = Column(String)
+	precio = Column(Integer)
