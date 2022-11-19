701b3e7298bf5179589a68be1813ab7f78415dc4
juan-fdo <arameo100@hotmail.com>
2022-11-19 18:08:31 -0500

    cambio en la contraseña

--- app/db/database.py
+++ app/db/database.py
@@ -2,6 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgresql://postgres:admin@localhost/tienda")
+engine = create_engine("postgresql://postgres:1986@localhost/tienda")
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 Base = declarative_base()
